f1ccdde490ab01aadb12a7009e662e3035683fee
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-08 02:12:06 -0300

    feat: create pokeCard component

---
+++ lib/screens/home/components/poke_card.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_project/pokemons/pokemons.dart';
+import 'package:pokedex_project/screens/details/details.dart';
+import 'package:pokedex_project/services/pokemon_service.dart';
+
+class PokeCard extends StatelessWidget {
+  const PokeCard(
+      {super.key, required this.name, required this.image, required this.id});
+  final String name;
+  final String image;
+  final int id;
+
+//TODO: PUT NAME, IMAGE AND ID
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: InkWell(
+          child: Card(
+            color: Colors.indigo[600],
+            //TODO: MAKE ALL CARDS RESPONSIBLE
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        name,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ), //TODO: NAME
+                      Text(
+                        '$id',
+                        style: TextStyle(color: Colors.white38),
+                      ), //TODO: ID
+                    ],
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 5.0),
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.all(3),
+                                width: 50,
+                                height: 25,
+                                decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Text(
+                                  'grass',
+                                  overflow: TextOverflow.ellipsis,
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(3),
+                              width: 50,
+                              height: 25,
+                              decoration: BoxDecoration(
+                                color: Colors.purple,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(
+                                'poison',
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          width: 70,
+                          height: 120,
+                          child: Image.network(
+                            image,
+                            fit: BoxFit.cover,
+                          ),
+                        ), //TODO: IMAGE
+                        // Image.network(image),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          onTap: () {
+            Navigator.pushNamed(context, 'details');
+          }),
+    );
+  }
+}
