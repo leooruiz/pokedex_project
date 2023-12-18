3892abce99bfa33a6d85488424157fae89ae2dd3
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-18 14:39:04 -0300

    feat: :sparkles: create details screen

--- lib/screens/details/details.dart
+++ lib/screens/details/details.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex_project/id_text.dart';
+import 'package:pokedex_project/pokemons/pokemons.dart';
+import 'package:pokedex_project/screens/details/components/about.dart';
+import 'package:pokedex_project/screens/details/components/base_stats.dart';
+import 'package:pokedex_project/screens/details/components/types.dart';
+import 'package:pokedex_project/theme/type_color.dart';
 
 class Details extends StatelessWidget {
-  const Details({super.key});
+  const Details({
+    super.key,
+    required this.pokemon,
+  });
+  final Pokemon pokemon;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: typeColor(pokemon.type1),
+        title: Text(
+          pokemon.name.toString().replaceFirst(
+                pokemon.name.toString()[0],
+                pokemon.name.toString()[0].toUpperCase(),
+              ),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child:
+                idText(pokemon, theme: Theme.of(context).textTheme.titleLarge),
+          )
+        ],
+      ),
+      body: Column(children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            width: double.infinity,
+            color: typeColor(pokemon.type1),
+            child: Image.network(
+              pokemon.bigImage,
+              fit: BoxFit.fitHeight,
+              filterQuality: FilterQuality.high,
+            ),
+          ),
+        ),
+        Expanded(
+            flex: 2,
+            child: Container(
+              color: Theme.of(context).primaryColorDark,
+              child: Column(
+                children: [
+                  DescriptionTypes(pokemon: pokemon),
+                  About(pokemon: pokemon),
+                  BaseStats(pokemon: pokemon),
+                ],
+              ),
+            ))
+      ]),
     );
   }
 }
