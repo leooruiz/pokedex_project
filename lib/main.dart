a1e2e2007269c20059e077abebc6c6c21b8598bd
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-18 14:44:17 -0300

    feat: :sparkles: add onGenerateRout and arguments to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex_project/pokemons/pokemons.dart';
 import 'package:pokedex_project/screens/details/details.dart';
 import 'package:pokedex_project/services/pokemon_service.dart';
 import 'package:pokedex_project/theme/my_theme.dart';
@@ -20,7 +21,37 @@ class PokedexApp extends StatelessWidget {
       theme: myTheme,
       initialRoute: 'home',
       routes: {
-        'details': (context) => const Details(),
+        'home': (context) => const Home(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == 'details') {
+          final Pokemon pokemon = settings.arguments as Pokemon;
+          return MaterialPageRoute(
+            builder: (context) {
+              return Details(
+                pokemon: Pokemon(
+                  name: pokemon.name,
+                  image: pokemon.image,
+                  id: pokemon.id,
+                  type1: pokemon.type1,
+                  type2: pokemon.type2,
+                  bigImage: pokemon.bigImage,
+                  hp: pokemon.hp,
+                  attack: pokemon.attack,
+                  defense: pokemon.defense,
+                  specialAttack: pokemon.specialAttack,
+                  specialDefense: pokemon.specialDefense,
+                  speed: pokemon.speed,
+                  weight: pokemon.weight,
+                  height: pokemon.height,
+                  move1: pokemon.move1,
+                  move2: pokemon.move2,
+                ),
+              );
+            },
+          );
+        }
+        return null;
       },
     );
   }
