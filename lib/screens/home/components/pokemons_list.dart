702fab9969f079389bb86b8cc1561f54a1c5ebbe
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-21 09:59:37 -0300

    perf: :zap: add initState to pokemons list and remove future builder

--- lib/screens/home/components/pokemons_list.dart
+++ lib/screens/home/components/pokemons_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_project/get_pokemon.dart';
-import 'package:pokedex_project/pokemons/pokemons.dart';
+import 'package:pokedex_project/pokemon/pokemon.dart';
 
 import 'poke_card.dart';
 
@@ -14,41 +14,43 @@ class PokemonsList extends StatefulWidget {
 }
 
 class _PokemonsListState extends State<PokemonsList> {
+  List<Pokemon> pokemons = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    pokemonsList();
+  }
+
+  void pokemonsList() async {
+    pokemons = await getPokemons();
+    isLoading = false;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: FutureBuilder(
-        future: getPokemons(),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(
-                  color: Theme.of(context).progressIndicatorTheme.color),
-            );
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('connection to the server was lost'));
-          } else {
-            List<Map> pokemons = snapshot.data;
-            return CustomScrollView(
-              slivers: [
-                SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, int index) {
-                      return PokeCard(
+    if (isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return CustomScrollView(
+      slivers: [
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            (context, int index) {
+              return PokeCard(
                 pokemon: pokemons[index],
-                      );
-                    },
-                    childCount: pokemons.length,
-                  ),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                ),
-              ],
-            );
-          }
-        },
-      ),
+              );
+            },
+            childCount: pokemons.length,
+          ),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+        ),
+      ],
     );
   }
 }
