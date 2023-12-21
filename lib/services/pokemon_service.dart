76ad7692bf46bbc5bcc75395b81ce8dfe639198e
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-21 10:02:25 -0300

    perf: :zap: update API to call just one time

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -1,48 +1,40 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/http.dart';
-import 'package:pokedex_project/pokemons/pokemons.dart';
+import 'package:pokedex_project/pokemon/pokemon.dart';
 import 'package:pokedex_project/services/http_interceptor.dart';
 
 class PokemonService {
-  String mainUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  String url = 'https://pokeapi.co/api/v2/pokemon/';
 
   http.Client client = InterceptedClient.build(interceptors: [
     LoggingInterceptor(),
   ]);
 
-  Future<List<String>> getUrl(String url) async {
-    List<String> urlList = [];
-    var i = 1;
-    http.Response response = await client.get(Uri.parse('$url$i'));
-    for (var i = 1; i < 30; i++) {
-      if (response.statusCode > 199 && response.statusCode < 300) {
-        response = await client.get(Uri.parse('$url$i'));
-        String localUrl = '$url$i';
-        urlList.add(localUrl);
-      } else {
-        throw HttpException;
-      }
+  Future<List<Uri>> getUrl(String url) async {
+    List<Uri> urlList = [];
+    for (var i = 1; i <= 50; i++) {
+      Uri localUrl = Uri.parse('$url$i');
+      urlList.add(localUrl);
     }
     return urlList;
   }
 
   Future<List<Pokemon>> getAll() async {
-    List mainUrlList =
-        await getUrl(mainUrl);
+    List urlList = await getUrl(url);
     List<Pokemon> pokemonList = [];
 
-    for (int i = 0; i < mainUrlList.length; i++) {
-      http.Response mainResponse = await client
-          .get(Uri.parse(mainUrlList[i]));
-
-      Map<String, dynamic> mainPokemonMap = json.decode(mainResponse.body);
-      pokemonList.add(Pokemon.fromMap(mainPokemonMap));
+    for (int i = 0; i < urlList.length; i++) {
+      http.Response response = await client.get(urlList[i]);
+      if (response.statusCode > 199 && response.statusCode < 300) {
+        Map<String, dynamic> pokemonMap = json.decode(response.body);
+        pokemonList.add(Pokemon.fromMap(pokemonMap));
+      } else {
+        throw ResponseException();
+      }
     }
     return pokemonList;
   }
 }
 
-class ResponseException {
-}
+class ResponseException implements Exception{}
