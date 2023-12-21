d54fe1f81f728483bb41b5df943022a0034b93bf
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-21 09:43:52 -0300

    refactor: :art: change helpers structure

--- lib/helpers/helper.dart
+++ lib/helpers/helper.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:pokedex_project/pokemons/pokemons.dart';
+import 'package:pokedex_project/pokemon/pokemon.dart';
 
 Text idText(Pokemon pokemon, {TextStyle? theme}) {
   switch (pokemon.id.toString().length) {
     case 1:
       return Text(
-        '${pokemon.id}'.padLeft(2, '#00'),
+        '#${pokemon.id.toString().padLeft(2, '00')}',
         style: theme,
       );
     case 2:
       return Text(
-        '${pokemon.id}'.padLeft(3, '#0'),
+        '#${pokemon.id.toString().padLeft(3, '0')}',
         style: theme,
       );
     default:
       return Text(
-        '${pokemon.id}'.padLeft(4, '#'),
+        '#${pokemon.id}',
         style: theme,
       );
   }
 }
-String pokemonNameUpperFirst(Pokemon pokemon) {
-      String name = pokemon.name
-          .replaceFirst(RegExp(pokemon.name[0]), pokemon.name[0].toUpperCase());
-      return name;
-    }
+
+String pokemonNameUpperFirst({required Pokemon pokemon}) {
+  String name = pokemon.name
+      .replaceFirst(RegExp(pokemon.name[0]), pokemon.name[0].toUpperCase());
+  return name;
+}
