303ac1f6fbac667408102ab4c6cd50331fd31fa5
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-21 09:52:31 -0300

    refactor: :art: replace types to widget

--- lib/screens/details/components/types.dart
+++ lib/screens/details/components/types.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pokedex_project/pokemons/pokemons.dart';
-import 'package:pokedex_project/theme/type_color.dart';
+import 'package:pokedex_project/pokemon/pokemon.dart';
+
+import 'details_type.dart';
 
 class DescriptionTypes extends StatelessWidget {
   const DescriptionTypes({super.key, required this.pokemon});
@@ -9,42 +10,14 @@ class DescriptionTypes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: typeColor(pokemon.type1),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            height: 40,
-                            width: 80,
-                            alignment: Alignment.center,
-                            child: Text(
-                              pokemon.type1,
-                            ),
-                          ),
-                        ),
-                        pokemon.type2 != null
-                            ? Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: typeColor(pokemon.type2),
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  height: 40,
-                                  width: 80,
-                                  alignment: Alignment.center,
-                                  child: Text(pokemon.type2!),
-                                ),
-                              )
-                            : const SizedBox(),
-                      ],
-                    ),
-                  );
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DetailsType(pokemonType: pokemon.type1),
+          DetailsType(pokemonType: pokemon.type2),
+        ],
+      ),
+    );
   }
 }
