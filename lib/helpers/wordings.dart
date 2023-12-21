132dd94137d9474f6a476cc3b7fc689e8fc089f6
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-21 09:45:18 -0300

    refactor: :art: create wordings class to strings

---
+++ lib/helpers/wordings.dart
@@ -0,0 +1,15 @@
+class Wordings {
+  String pokedexFirstUpper = 'Pokedéx';
+  String pokedexFirstLower = 'pokedéx';
+  String hp = 'HP';
+  String atk = 'ATK';
+  String def = 'DEF';
+  String satk = 'SATK';
+  String sdef = 'SDEF';
+  String spd = 'SPD';
+  String weight = 'weight';
+  String height = 'height';
+  String mainMoves = 'Main moves';
+  String baseStats = 'Base Stats';
+  String about = 'About';
+}
