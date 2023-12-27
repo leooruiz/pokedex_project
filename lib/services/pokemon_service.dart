38da4b1fdb22243ff6314550a8b908c84bcd2bbf
Leonardo <leoruizz14@outlook.com>
2023-12-27 19:53:05 -0300

    feat: :sparkles: implement lazy load to pokemonsList

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -5,26 +5,25 @@ import 'package:pokedex_project/pokemon/pokemon.dart';
 import 'package:pokedex_project/services/http_interceptor.dart';
 
 class PokemonService {
-  String url = 'https://pokeapi.co/api/v2/pokemon/';
+  static const String url = 'https://pokeapi.co/api/v2/pokemon/';
+  final List<Uri> urlList = [];
 
   http.Client client = InterceptedClient.build(interceptors: [
     LoggingInterceptor(),
   ]);
 
-  Future<List<Uri>> getUrl(String url) async {
-    List<Uri> urlList = [];
-    for (var i = 1; i <= 50; i++) {
+  Future<List<Uri>> getUrl(String url, int pokemonAmount) async {
+    for (var i = pokemonAmount-19; i <= pokemonAmount; i++) {
       Uri localUrl = Uri.parse('$url$i');
       urlList.add(localUrl);
     }
     return urlList;
   }
 
-  Future<List<Pokemon>> getAll() async {
-    List urlList = await getUrl(url);
+  Future<List<Pokemon>> getAll(int pokemonAmount) async {
+    List urlList = await getUrl(url, pokemonAmount);
     List<Pokemon> pokemonList = [];
-
-    for (int i = 0; i < urlList.length; i++) {
+    for (var i = 0; i < urlList.length; i++) {
       http.Response response = await client.get(urlList[i]);
       if (response.statusCode > 199 && response.statusCode < 300) {
         Map<String, dynamic> pokemonMap = json.decode(response.body);
@@ -37,4 +36,4 @@ class PokemonService {
   }
 }
 
-class ResponseException implements Exception{}
+class ResponseException implements Exception {}
