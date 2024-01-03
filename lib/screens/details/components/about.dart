29bace7783328991e6475e5d102fe2d9a0d103b7
Leonardo Ruiz <leoruizz14@outlook.com>
2024-01-03 17:49:50 -0300

    feat: change Kg to g in details

--- lib/screens/details/components/about.dart
+++ lib/screens/details/components/about.dart
@@ -22,88 +22,88 @@ class About extends StatelessWidget {
           ),
         ),
         Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 5.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    Wordings.weight,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          Wordings.weight,
                           style: Theme.of(context).textTheme.bodyMedium,
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Text(
-                              '${pokemon.weight}.0Kg',
-                              style: Theme.of(context).textTheme.bodySmall,
-                            ),
-                          ],
                         ),
-                      ),
-                      Container(
-                        width: 1,
-                        height: 50,
-                        color: typeColor(pokemon.type1),
-                      ),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 5.0),
-                              child: Text(
-                                Wordings.height,
-                                style: Theme.of(context).textTheme.bodyMedium,
-                              ),
-                            ),
-                            Text(
-                              '${pokemon.height}.0cm',
-                              style: Theme.of(context).textTheme.bodySmall,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 1,
-                        height: 50,
-                        color: typeColor(pokemon.type1),
-                      ),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 5.0),
-                              child: Text(
-                                Wordings.mainMoves,
-                                style: Theme.of(context).textTheme.bodyMedium,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 3.0),
-                              child: Text(
-                                pokemon.move1,
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                            ),
-                            pokemon.move2 != null
-                                ? Text(
-                                    pokemon.move2!,
-                          style: Theme.of(context).textTheme.bodySmall,
-                                  )
-                                : const SizedBox(),
-                          ],
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
+                  Text(
+                    '${pokemon.weight} g',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 1,
+              height: 50,
+              color: typeColor(pokemon.type1),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5.0),
+                    child: Text(
+                      Wordings.height,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                  Text(
+                    '${pokemon.height}.0 cm',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 1,
+              height: 50,
+              color: typeColor(pokemon.type1),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5.0),
+                    child: Text(
+                      Wordings.mainMoves,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 3.0),
+                    child: Text(
+                      pokemon.move1,
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ),
+                  pokemon.move2 != null
+                      ? Text(
+                          pokemon.move2!,
+                          style: Theme.of(context).textTheme.bodySmall,
+                        )
+                      : const SizedBox(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
